799bbcacc2d52b32f6c069894932567bf54f7db5
skshakya999 <skshakya999@gmail.com>
2022-06-21 15:55:11 +0530

    Start by Sumit

--- src/models/authorModel.js
+++ src/models/authorModel.js
@@ -1,11 +1,31 @@
 const mongoose = require('mongoose');
 
 const authorSchema = new mongoose.Schema( {
-    
-    authorName: String,
-    age:Number,
-    address:String,
-    rating:Number
+    fname:{
+        type:String,
+        required:true
+    },
+    lname:{
+        type:String,
+        required:true
+    },
+    title:{
+        type:String,
+        required:true,
+        enum:['Mr','Mrs','Miss']
+    },
+    email:{
+        type:String,
+        match: /.+\@.+\..+/,
+        required:true,
+        unique:true
+
+    },
+    password:{
+        type:String,
+        required:true
+    }
+  
 
 }, { timestamps: true });
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,27 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const authorController= require("../controllers/authorController")
-const bookController= require("../controllers/bookController")
-const publisherController= require("../controllers/publisherController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
 
-router.post("/createAuthor", authorController.createAuthor  )
+router.post("/createAuthor", authorController.createAuthor)
 
-router.get("/getAuthorsData", authorController.getAuthorsData)
 
-router.post("/createBook", bookController.createBook  )
-
-router.put("/updateBooksData", bookController.updateBooksData)
-
-router.put("/updateBooksPrice", bookController.updateBooksPrice)
-
-router.post("/createPublisher", publisherController.createPublisher)
-
-router.get("/getPublisherData", publisherController.getPublisherData)
-
-router.get("/getBooksWithAuthorDetails", bookController.getBooksWithAuthorDetails)
 
 module.exports = router;
