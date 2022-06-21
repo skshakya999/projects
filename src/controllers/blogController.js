4c37677ff8803ccf3c098350e6f8da531b81d617
skshakya999 <skshakya999@gmail.com>
2022-06-22 00:04:24 +0530

    Delete post api by Sumit

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -1,32 +1,66 @@
-const BlogModel= require("../models/blogModel");
+const BlogModel = require("../models/blogModel");
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
-const createBlog= async function (req, res) {
+const createBlog = async function (req, res) {
     let blog = req.body
     let authorCreated = await BlogModel.create(blog)
-    res.send({data: authorCreated})
+    res.send({ data: authorCreated })
 }
 
-const validId = function(ObjectId){ return mongoose.Types.ObjectId.isValid(ObjectId)}
-const updateBlog = async function(req,res){
+const validId = function (ObjectId) { return mongoose.Types.ObjectId.isValid(ObjectId) }
 
-   try {
+
+const updateBlog = async function (req, res) {
+
+    try {
         const blogId = req.params.blogId
-    if(!validId(blogId)) return res.status(400).send({status:false,msg:"Invalid blog id!"})
-    let blog = await BlogModel.findOne({_id:blogId,isDeleted:false})
-    if(!blog) return res.status(404).send({status:false,msg:"No blog found!"})
+        if (!validId(blogId)) return res.status(400).send({ status: false, msg: "Invalid blog id!" })
+        let blog = await BlogModel.findOne({ _id: blogId, isDeleted: false })
+        if (!blog) return res.status(404).send({ status: false, msg: "No blog found!" })
 
-    let data = req.body
+        let data = req.body
 
-    let updatedData = await BlogModel.findOneAndUpdate({_id:blogId,isDeleted:false}, data,{new:true})
-    res.status(200).send({blog:updatedData})
+        let updatedData = await BlogModel.findOneAndUpdate({ _id: blogId, isDeleted: false }, data, { new: true })
+        res.status(200).send({ blog: updatedData })
+    }
+    catch (err) {
+        res.status(400).send({ error: err })
+    }
 }
-catch(err){
-    res.status(400).send({error:err})
+
+const deletePost = async function (req, res) {
+    try {
+        const blogId = req.params.blogId
+        if (!validId(blogId)) return res.status(400).send({ status: false, msg: "Invalid blog id!" })
+        let blog = await BlogModel.findOne({ _id: blogId, isDeleted: false })
+        if (!blog) return res.status(404).send({ status: false, msg: "No blog found!" })
+
+        let deletedData = await BlogModel.findOneAndUpdate({ _id: blogId, isDeleted: false }, { isDeleted: true }, { new: true })
+        res.status(200).send({ blog: deletedData })
+    }
+    catch (err) {
+        res.status(400).send({ status: false, error: err })
+    }
 }
+
+const deletePostQuery = async function(req,res){
+    
+    let category =req.query.category
+    let authorid =req.query.authorid
+    let tag =req.query.tag
+    let subcategory =req.query.subcategory
+    let unpublished =req.query.unpublished
+
+    if (!validId(authorid)) return res.status(400).send({ status: false, msg: "Invalid author id!" })
+    let deletedData = await BlogModel.findOneAndUpdate({ category:{$eleMatch:{$eq:category}},authorId:authorid,tags:{$eleMatch:{$eq:tag}},subcategory:{$eleMatch:{$eq:subcategory}}, isPublished: unpublished }, { isDeleted: true }, { new: true })
+    res.status(200).send({ blog: deletedData })
+
+
 }
 
 
-module.exports.createBlog= createBlog
-module.exports.updateBlog= updateBlog
+module.exports.createBlog = createBlog
+module.exports.updateBlog = updateBlog
+module.exports.deletePost = deletePost
+module.exports.deletePostQuery = deletePostQuery
--- src/routes/route.js
+++ src/routes/route.js
@@ -7,6 +7,8 @@ const blogController= require("../controllers/blogController")
 router.post("/authors", authorController.createAuthor)
 router.post("/createBlog", blogController.createBlog)
 router.put("/blogs/:blogId", blogController.updateBlog)
+router.post("/blogs/:blogId", blogController.deletePost)
+// router.post("/blogs", blogController.deletePostQuery)
 
 
 module.exports = router;
