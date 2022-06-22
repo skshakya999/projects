eff278a3cca570d719bbc078ca00af9cf6803482
skshakya999 <skshakya999@gmail.com>
2022-06-22 13:35:42 +0530

    last edited api

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -2,13 +2,17 @@ const BlogModel = require("../models/blogModel");
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
+const validId = function (ObjectId) { return mongoose.Types.ObjectId.isValid(ObjectId) }
+
 const createBlog = async function (req, res) {
     let blog = req.body
+    let authorid =blog.authorId
     let authorCreated = await BlogModel.create(blog)
-    res.send({ data: authorCreated })
+
+    res.status(200).send({ data: authorCreated })
 }
 
-const validId = function (ObjectId) { return mongoose.Types.ObjectId.isValid(ObjectId) }
+
 
 
 const updateBlog = async function (req, res) {
@@ -16,12 +20,11 @@ const updateBlog = async function (req, res) {
     try {
         const blogId = req.params.blogId
         if (!validId(blogId)) return res.status(400).send({ status: false, msg: "Invalid blog id!" })
-        let blog = await BlogModel.findOne({ _id: blogId, isDeleted: false })
-        if (!blog) return res.status(404).send({ status: false, msg: "No blog found!" })
-
+        
         let data = req.body
 
         let updatedData = await BlogModel.findOneAndUpdate({ _id: blogId, isDeleted: false }, data, { new: true })
+        if (!updatedData) return res.status(404).send({ status: false, msg: "No blog found!" })
         res.status(200).send({ blog: updatedData })
     }
     catch (err) {
